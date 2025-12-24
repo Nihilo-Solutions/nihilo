bc85bbd09e550fcacb3d5f3f43e45569a32b1384
Sam Oakes <samoakescyber@proton.me>
2025-12-23 21:29:53 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import AssessmentPage from './pages/AssessmentPage';
+import SecurityPage from './pages/SecurityPage'; // The missing import
 
-const App: React.FC = () => {
+function App() {
   return (
-    <Router>
-      <div className="relative min-h-screen bg-zinc-950">
-        <Navbar /> {/* Navbar stays at top across all pages */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/assessment" element={<AssessmentPage />} />
-          <Route path="/security" element={<SecurityPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/assessment" element={<AssessmentPage />} />
+      <Route path="/security" element={<SecurityPage />} /> {/* The new route */}
+    </Routes>
   );
-};
+}
 
 export default App;
