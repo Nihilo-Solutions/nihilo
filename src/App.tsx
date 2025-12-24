fd3d75ae4cdb1309b73ed463ad4b3fafe51e9c27
Sam Oakes <samoakescyber@proton.me>
2025-12-23 21:32:24 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+// Added BrowserRouter to the import list below
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import AssessmentPage from './pages/AssessmentPage';
-import SecurityPage from './pages/SecurityPage'; // The missing import
+import SecurityPage from './pages/SecurityPage';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/assessment" element={<AssessmentPage />} />
-      <Route path="/security" element={<SecurityPage />} /> {/* The new route */}
-    </Routes>
+    /* This wrapper is what was missing and causing the error */
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/assessment" element={<AssessmentPage />} />
+        <Route path="/security" element={<SecurityPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
